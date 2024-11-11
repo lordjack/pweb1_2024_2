e06d87f75cd9cf054c0fce34690568dc4e13500d
Jackson Meires <lordjackson@gmail.com>
2024-11-11 17:19:48 -0300

    Exemplo aula PHP diretivas e form

---
+++ php/site/noticias.php
@@ -0,0 +1,21 @@
+<?php
+ include "./header.php";
+?>
+
+<div class="row">
+    <div class="col">
+        <h2>Notícias</h2>
+        
+        <h2>Chapecoense na serie A</h2>
+        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur officiis, saepe ex, repellendus eos quis itaque sunt molestias labore dolorum accusamus. Ratione excepturi et aut deserunt soluta sapiente tempora itaque.</p>
+
+        <h2>EcoPark Recebe Tobogam</h2>
+        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur officiis, saepe ex, repellendus eos quis itaque sunt molestias labore dolorum accusamus. Ratione excepturi et aut deserunt soluta sapiente tempora itaque.</p>
+
+    </div>
+</div>
+
+
+<?php
+ include "./footer.php";
+?>
---
+++ php/site/sobre.php
@@ -0,0 +1,17 @@
+<?php
+ include "./header.php";
+?>
+
+<div class="row">
+    <div class="col">
+        <h2>Sobre</h2>
+        <p class="text-center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores atque rem totam omnis officiis. Incidunt eveniet totam quas modi eos. Iusto officiis ut aut illo magni perspiciatis quod sequi quidem.
+        </p>
+    </div>
+</div>
+
+
+<?php
+ include "./footer.php";
+?>
