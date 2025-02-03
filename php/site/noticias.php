ed01fee1ce7d9c78a28c4e30920140a21e85165f
jackson <lordjackson@gmail.com>
2025-02-03 18:18:27 -0300

    add listagem de noticias no blog do site

--- php/site/noticias.php
+++ php/site/noticias.php
@@ -1,17 +1,23 @@
 <?php
  include "./header.php";
+ include "./admin/db.class.php";
 ?>
 
 <div class="row">
     <div class="col">
         <h2>Notícias</h2>
         
-        <h2>Chapecoense na serie A</h2>
-        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur officiis, saepe ex, repellendus eos quis itaque sunt molestias labore dolorum accusamus. Ratione excepturi et aut deserunt soluta sapiente tempora itaque.</p>
+        <?php
 
-        <h2>EcoPark Recebe Tobogam</h2>
-        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur officiis, saepe ex, repellendus eos quis itaque sunt molestias labore dolorum accusamus. Ratione excepturi et aut deserunt soluta sapiente tempora itaque.</p>
+            $db = new db('post');
+            $posts = $db->all();
 
+            foreach($posts as $item){
+                echo "<h2>$item->titulo</h2>";
+                echo "<h6>Publicado em: ".date('d/m/Y H:i', strtotime($item->data_publicacao))."</h6>";
+                echo "<p>$item->texto</p>";
+            }
+        ?>
     </div>
 </div>
 
